626190c1b498178e1c12a8043b4288d6f9d9eafd
Philip Nguyen <ng.philip.91@gmail.com>
2020-11-28 15:07:19 +0100

    ADD basic functionality: make snapshot with webcam

--- src/app/modules/launcher/pages/launcher-home/launcher-home.component.ts
+++ src/app/modules/launcher/pages/launcher-home/launcher-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as playerReducer from 'src/app/shared/reducers/player.reducer';
 import * as playerActions from 'src/app/shared/actions/player.actions';
@@ -10,6 +10,13 @@ import * as playerActions from 'src/app/shared/actions/player.actions';
 })
 export class LauncherHomeComponent implements OnInit {
   nameInput = '';
+  imgURL = null;
+
+  @ViewChild('video', { static: true })
+  video: ElementRef;
+
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef;
 
   constructor(private playerStore: Store<playerReducer.State>) {}
 
@@ -17,9 +24,44 @@ export class LauncherHomeComponent implements OnInit {
     this.playerStore.select(playerReducer.selectCurrPlayerName).subscribe((currPlayer) => {
       this.nameInput = currPlayer;
     });
+
+    this.canvas.nativeElement.width = 300;
+    this.canvas.nativeElement.height = 200;
+    this.video.nativeElement.width = 300;
+    this.video.nativeElement.height = 200;
+    this.runWebcam();
+  }
+
+  async runWebcam() {
+    try {
+      this.video.nativeElement.srcObject = await navigator.mediaDevices.getUserMedia({ video: {} });
+    } catch (e) {
+      console.error('error running webcam', e);
+    }
   }
 
   saveUser() {
     this.playerStore.dispatch(playerActions.saveCurrPlayer({ currPlayer: this.nameInput.trim() }));
+  }
+
+  onSnapshot() {
+    // this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
+    // this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
+
+    const sourceImage = this.video.nativeElement;
+    const sourceX = 0;
+    const sourceY = 0;
+    const sourceWidth = this.video.nativeElement.videoWidth;
+    const sourceHeight = this.video.nativeElement.videoHeight;
+    const canvasX = 0;
+    const canvasY = 0;
+    const canvasWidth = this.canvas.nativeElement.width;
+    const canvasHeight = this.canvas.nativeElement.height;
+
+    this.canvas.nativeElement
+      .getContext('2d')
+      .drawImage(sourceImage, sourceX, sourceY, sourceWidth, sourceHeight, canvasX, canvasY, canvasWidth, canvasHeight);
+
+    this.imgURL = this.canvas.nativeElement.toDataURL('image/png');
   }
 }
--- src/app/shared/effects/game.effects.ts
+++ src/app/shared/effects/game.effects.ts
@@ -3,18 +3,16 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
-import { catchError, combineAll, exhaustMap, map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { catchError, combineAll, exhaustMap, map, switchMap } from 'rxjs/operators';
 import {
   CreateGame,
   CREATE_GAME,
-  QueryGame,
+  FAILED,
   QueryGames,
-  QUERY_GAME,
   QUERY_GAMES,
   success,
   UPDATE_GAMES,
 } from '../actions/game.actions';
-import { FAILED } from '../actions/game.actions';
 import { SET_PARTY_STEP } from '../actions/party.actions';
 import { GameFsService } from '../firestore-services/game-fs.service';
 import { PartyFsService } from '../firestore-services/party-fs.service';
