0e8110c381250f22bfb3a2638e8a06092012f067
Philip Nguyen <ng.philip.91@gmail.com>
2020-12-06 11:04:06 +0100

    ADD snapshot with "Schablone"

--- src/app/modules/lobby/lobby-avatar-creator/lobby-avatar-creator.component.ts
+++ src/app/modules/lobby/lobby-avatar-creator/lobby-avatar-creator.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
@@ -13,12 +13,28 @@ import { LobbyParentComponent } from '../lobby-parent/lobby-parent.component';
   templateUrl: './lobby-avatar-creator.component.html',
   styleUrls: ['./lobby-avatar-creator.component.css'],
 })
-export class LobbyAvatarCreatorComponent extends LobbyParentComponent implements OnInit {
+export class LobbyAvatarCreatorComponent extends LobbyParentComponent implements OnInit, AfterViewInit {
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event) {
+    this.updateImageSizes();
+  }
+
+  @ViewChild('container')
+  container: ElementRef;
+
   @ViewChild('video', { static: true })
   video: ElementRef;
 
+  @ViewChild('faceTemplate', { static: true })
+  faceTemplate: ElementRef;
+
   @ViewChild('canvas', { static: true })
   canvas: ElementRef;
+
+  @ViewChild('snapshot', { static: true })
+  snapshot: ElementRef;
+  snapshotHeight;
+  snapshotWidth;
 
   imgURL = null;
   currPlayerAvatar: File = null;
@@ -29,17 +45,14 @@ export class LobbyAvatarCreatorComponent extends LobbyParentComponent implements
     protected store: Store,
     private images: ImageFsService,
     private actions$: Actions,
-    private router: Router
+    private router: Router,
+    private renderer: Renderer2
   ) {
     super(route, store);
   }
 
   ngOnInit(): void {
     super.ngOnInit();
-    this.canvas.nativeElement.width = 300;
-    this.canvas.nativeElement.height = 200;
-    this.video.nativeElement.width = 300;
-    this.video.nativeElement.height = 200;
     this.runWebcam();
 
     this.actions$
@@ -47,36 +60,68 @@ export class LobbyAvatarCreatorComponent extends LobbyParentComponent implements
       .subscribe(() => this.router.navigate([`lobby/${this.partyName}/${this.playerFireId}`]));
   }
 
+  ngAfterViewInit() {}
+
   async runWebcam() {
     try {
       this.video.nativeElement.srcObject = await navigator.mediaDevices.getUserMedia({ video: {} });
+      this.updateImageSizes();
     } catch (e) {
       console.error('error running webcam', e);
     }
   }
 
   onSnapshot() {
-    // this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
-    // this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
-
     const sourceImage = this.video.nativeElement;
-    const sourceX = 0;
-    const sourceY = 0;
-    const sourceWidth = this.video.nativeElement.videoWidth;
-    const sourceHeight = this.video.nativeElement.videoHeight;
-    const canvasX = 0;
-    const canvasY = 0;
-    const canvasWidth = this.canvas.nativeElement.width;
-    const canvasHeight = this.canvas.nativeElement.height;
+    const faceTemplate = this.faceTemplate.nativeElement;
+
+    const a = (sourceImage.videoWidth - sourceImage.width) / 2;
+    const b = (sourceImage.width - faceTemplate.width) / 2;
+    const sourceX = a + b;
+    const c = (sourceImage.videoHeight - sourceImage.height) / 2;
+    const d = (sourceImage.height - faceTemplate.height) / 2;
+    const sourceY = c + d;
+    // const sourceY =
+    // this.faceTemplate.nativeElement.getBoundingClientRect().top -
+    // this.video.nativeElement.getBoundingClientRect().top;
 
     this.canvas.nativeElement
       .getContext('2d')
-      .drawImage(sourceImage, sourceX, sourceY, sourceWidth, sourceHeight, canvasX, canvasY, canvasWidth, canvasHeight);
+      .drawImage(
+        sourceImage,
+        sourceX,
+        sourceY,
+        faceTemplate.width,
+        faceTemplate.height,
+        0,
+        0,
+        faceTemplate.width,
+        faceTemplate.height
+      );
 
+    this.snapshotHeight = faceTemplate.height;
+    this.snapshotWidth = faceTemplate.width;
     this.imgURL = this.canvas.nativeElement.toDataURL('image/jpg');
     this.canvas.nativeElement.toBlob((blob) => {
       this.currPlayerAvatar = new File([blob], 'snapshot.png', { type: 'image/png' });
     }, 'image/png');
+  }
+
+  private updateImageSizes() {
+    const totalWidth = Math.round(window.outerWidth);
+
+    this.renderer.setStyle(this.container.nativeElement, 'height', `${totalWidth}px`);
+    this.renderer.setStyle(this.container.nativeElement, 'width', `${totalWidth}px`);
+    this.video.nativeElement.height = totalWidth;
+    this.video.nativeElement.width = totalWidth;
+
+    this.renderer.setStyle(this.faceTemplate.nativeElement, 'height', `${Math.round(totalWidth / 2)}px`);
+    this.renderer.setStyle(this.faceTemplate.nativeElement, 'top', '50%');
+    this.renderer.setStyle(this.faceTemplate.nativeElement, 'left', '50%');
+    this.renderer.setStyle(this.faceTemplate.nativeElement, 'transform', 'translateX(-50%) translateY(-50%)');
+
+    this.canvas.nativeElement.width = this.faceTemplate.nativeElement.width;
+    this.canvas.nativeElement.height = this.faceTemplate.nativeElement.height;
   }
 
   async onNext() {
